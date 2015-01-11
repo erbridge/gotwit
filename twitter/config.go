1c58250d355bdb802ce600b2e6c1ba8d38831ce8
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-01-11 17:22:48 +0000

    Add twitter.LoadConfig.

--- twitter/config.go
+++ twitter/config.go
@@ -1,5 +1,10 @@
 package twitter
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 type (
 	ConsumerConfig struct {
 		key    string
@@ -24,4 +29,17 @@ func NewAccessConfig(token string, tokenSecret string) AccessConfig {
 		token:       token,
 		tokenSecret: tokenSecret,
 	}
+}
+
+func LoadConfig(f string) (c ConsumerConfig, a AccessConfig, err error) {
+	var conf map[string]string
+	bytes, _ := ioutil.ReadFile(f)
+	if err = json.Unmarshal(bytes, &conf); err != nil {
+		return
+	}
+	c.key = conf["consumer_key"]
+	c.secret = conf["consumer_secret"]
+	a.token = conf["access_token"]
+	a.tokenSecret = conf["access_token_secret"]
+	return
 }
